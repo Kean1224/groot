a6cc11253c76b617dee059e3d47fdb7891476942
Kean1224 <keanmartin75@gmail.com>
2025-07-25 09:08:12 +0200

    Add frontend and backend folders

--- frontend/.next/types/app/page.ts
+++ frontend/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\keanm\OneDrive\Desktop\123\frontend_fixed\app\page.tsx
+// File: C:\Users\keanm\OneDrive\Desktop\123\frontend\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
